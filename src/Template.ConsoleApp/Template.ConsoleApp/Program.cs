ded488b6b94444b5fe853f6e1b32e239cb6c2c1a
Dan Sherwood <dsherwood@bowvalleycollege.ca>
2018-06-14 17:14:00 -0600

    initial commit

---
+++ src/Template.ConsoleApp/Template.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+
+namespace Template.ConsoleApp
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Configure();
+                Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "An exception was thrown while running the application.");
+            }
+            finally
+            {
+                Log.Information("Application closing.");
+                Log.CloseAndFlush();
+            }
+        }
+
+        private static void Configure()
+        {
+            var startup = new Startup();
+            Log.Information("Building Service Collection.");
+            IServiceCollection services = new ServiceCollection();
+            startup.ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        public static void Run()
+        {
+            
+        }
+    }
+}
---
+++ src/Template.ConsoleApp/Template.ConsoleApp/Startup.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.ConsoleApp
+{
+    public class Startup
+    {
+        private IConfigurationRoot Configuration { get; }
+
+        public Startup()
+        {
+            var environment = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(@"appSettings.Json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appSettings.{environment}.Json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+
+            Log.Information("Logging Initialized.");
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            Log.Information("Configuring Services.");
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
+            services.AddOptions();
+            services.AddAutoMapper();
+
+            Mapper.AssertConfigurationIsValid();
+
+        }
+    }
+}
